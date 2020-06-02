bc2356c17701d032a74e0dfb98ea687dd7d5b199
Sugyeong Kim <40874517+sugyeongKim@users.noreply.github.com>
2020-06-02 10:29:52 +0900

    5번 문제

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,40 @@ namespace overridemethos
 {
     class Program
     {
+        static int power(int input)
+        {
+            return input * input;
+        }
+
+        static int power(int input, int count)
+        {
+            int num = input;
+            for (int i = 1; i < count; i++)
+                num *= input;
+            return num;
+        }
+
+        static int SumAll(int end)
+        {
+            int sum = 0;
+            for (int i = 0; i <= end; i++)
+                sum += i;
+            return sum;
+        }
+
+        static int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+                sum += i;
+            return sum;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(power(3));
+            Console.WriteLine(power(3,4));
+            Console.WriteLine(SumAll(10));
+            Console.WriteLine(SumAll(2, 7));
         }
     }
 }
